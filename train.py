e870c055b9ccde4fc673495a9c5d1b04cb1ff25c
arjan <arjanbista@gmail.com>
2026-05-16 21:59:27 +0545

    Update

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from envs.robotic_arm_env import CustomEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback
+from datetime import datetime
 import os
 
 os.makedirs("models", exist_ok=True)
@@ -11,13 +12,15 @@ os.makedirs("logs", exist_ok=True)
 
 urdf="/home/arjan/arm_rl/urdf/robotic_arm/urdf/robotic_arm.urdf"
 path="models/ppo_robotic_arm"
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+log_path = f"./logs/base_{timestamp}/"
 # env=CustomEnv(urdf)
 # env=Monitor(env)  #Tracks reward for TensorBoard
 env=DummyVecEnv([lambda:Monitor(CustomEnv(urdf))])
 
 if os.path.exists(path+".zip"):
     print("Loading existing model...")
-    model = PPO.load(path, env=env)
+    model = PPO.load(path, env=env, tensorboard_log=log_path)
 else:
     print("Starting fresh...")
     model = PPO("MlpPolicy",
@@ -33,7 +36,7 @@ else:
                 n_steps=2048,
                 max_grad_norm=0.5,
                 device="cpu",
-                tensorboard_log="./logs/")
+                tensorboard_log=log_path)
 
 checkpoint_callback=CheckpointCallback(save_freq=5000, save_path="models/",name_prefix="ppo_robotic_arm")
 
